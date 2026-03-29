3f14163cb1e2abec55bada40b16f25c46caa534e
Dilshan Balasooriya <dilabnoname1245@gmail.com>
2026-03-30 03:17:23 +0530

    AI vision:can extract the plate number and plate image form the frames.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,16 @@
 import _asyncio
 from fastapi import FastAPI
 from contextlib import contextmanager
+from .managers.vision import VisionManager
 
 
 
 @contextmanager
 async def lifespan(app: FastAPI):
     print("Loading the Model")
-    ## Model logic here
+    vision = VisionManager(model_path="../best_openvino_model")
     yield
+    
     print("sutting down")
 
 app = FastAPI(lifespan=lifespan)
